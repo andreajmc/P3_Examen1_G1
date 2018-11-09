fb199c68165f5cf4bc2054a103eaeaabaa79942b
andreajmc <andreaj@unitec.edu>
2018-11-09 14:35:05 -0600

    idl

--- Main.cpp
+++ Main.cpp
@@ -1,17 +1,26 @@
 #include <ncurses.h>
 #include <unistd.h>
-#include <string>
+#include <string>i
 using std::string;
+// #include "Item.h"
+
+Item*** crearTablero();
+bool lvlOne(char*, int, Item***);
+bool lvlTwo(char*, int, Item***);
+bool lvlThree(char*, int, Item***);
+void finale(bool, char*);
+
 int main() {
     int x = 0;
     int y = 0;
     int xMax = 0;
     int yMax = 0;
     char name[20] = {""};
+    int vidas = 3;
     initscr();
     echo();
     curs_set(FALSE);
-    WINDOW* menuwin = newwin(10, 20, xMax / 2, yMax / 2); // height, width, starty, start
+    WINDOW* menuwin = newwin(8, 20, xMax / 2, yMax / 2); // height, width, starty, start
     box(menuwin, 0, 0); // windows, characters
     keypad(menuwin, true);
     string w = "¡Bienvenido!";
@@ -40,6 +49,7 @@ int main() {
         // Pantallita del juego.
             switch(choice) {
             case KEY_UP:
+                HL--;
                 if (HL < 0) {
                     HL = 0;
                 }
@@ -57,25 +67,52 @@ int main() {
             break;
         }
     }
-/*
-    switch(HL) {
-        case 0:
-            lvlOne(); // Parametro --> Name & Vidas
-            break;
-        case 1:
-            lvlTwo();
-            break;
-        case 2:
-            lvlThree();
-            break;
-        case 3:
-            break;
-    } */
+        WINDOW* mainGame = newwin(10, 10, 0, 0);
+        Item*** Board = crearTablero();
+        bool win = false;
+        switch(HL) {
+            case 0:
+                win = lvlOne(name, vidas, Board); // Parametro --> Name & Vidas
+                finale(win, name);
+                break;
+            case 1:
+                win = lvlTwo(name, vidas, Board);
+                finale(win, name);
+                break;
+            case 2:
+                win = lvlThree(name, vidas, Board);
+                finale(win, name);
+                break;
+            case 3:
+                break;
+        }
     }
     refresh();
     wrefresh(menuwin);
     delwin(menuwin);
     getch();
     endwin();
+}
+
+Item*** crearTablero() {
+    Item*** Board = new Item**[10];
+    for (int i = 0; i < 10; i++) {
+        Board[i] = new Item*[10];
+    }
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            Board[i][j] = NULL;
+        }
+    }
+    return Board;
+}
+
+void finale(bool win, char* name) {
+    clear();
+    if (win) {
+        
+    } else {
+
+    }
 }
 
